28c1972dd3e1185ae4666d50601428a07bb85bc6
David Reichert <wildcat1221@gmail.com>
2014-04-13 14:11:07 -0500

    Added half notes, function started for whole notes. Resized staff and all notes

--- public/JS/logic.js
+++ public/JS/logic.js
@@ -29,59 +29,99 @@ function drawMeasure(measureCount, measureIterator, lineCount) {
       iterateLine();
     }
   }
-  yAxisShift = 200 * (lineCount - 1);
+  yAxisShift = 125 * (lineCount - 1);
   xAxisShift = 300 * measureIterator;
 
-  var staveOuter = new Path.Rectangle(new Point(20 + xAxisShift, 150 + yAxisShift), new Point(320 + xAxisShift, 250 + yAxisShift)).strokeColor = "#000";
-  var ledgerOneStart = new Point(20 + xAxisShift, 250 + yAxisShift),
-        ledgerOneEnd  = new Point(320 + xAxisShift, 250 + yAxisShift);
+  var staveOuter = new Path.Rectangle(new Point(20 + xAxisShift, 173 + yAxisShift), new Point(320 + xAxisShift, 225 + yAxisShift)).strokeColor = "#000";
+  var ledgerOneStart = new Point(20 + xAxisShift, 225 + yAxisShift),
+        ledgerOneEnd  = new Point(320 + xAxisShift, 225 + yAxisShift);
   var ledgerOne = new Path.Line(ledgerOneStart, ledgerOneEnd);
-  var ledgerTwo = new Path.Line(ledgerOneStart + [0, -25], ledgerOneEnd + [0, -25]).strokeColor = "#000";
-  var ledgerThree = new Path.Line(ledgerOneStart + [0, -50], ledgerOneEnd + [0, -50]).strokeColor = "#000";
-  var ledgerFour = new Path.Line(ledgerOneStart + [0, -75], ledgerOneEnd + [0, -75]).strokeColor = "#000";
+  var ledgerTwo = new Path.Line(ledgerOneStart + [0, -13], ledgerOneEnd + [0, -13]).strokeColor = "#000";
+  var ledgerThree = new Path.Line(ledgerOneStart + [0, -26], ledgerOneEnd + [0, -26]).strokeColor = "#000";
+  var ledgerFour = new Path.Line(ledgerOneStart + [0, -39], ledgerOneEnd + [0, -39]).strokeColor = "#000";
 
   iterateMeasureCount();
   measureTracker();
 }
 
 function drawQuarter(x, y) {
-  var note = new Path.Ellipse(new Rectangle(new Point(x, y), new Size(18, 14))).rotate(-22.5).fillColor = "black";
+  var note = new Shape.Ellipse(new Rectangle(new Point(x, y), new Size(18, 14))).rotate(-22.5).fillColor = "black";
 
-  var stem = new Path.Line(new Point(x + 16, y + 8), new Point(x + 16, y - 40));
+  var stem = new Path.Line(new Point(x + 17, y + 6), new Point(x + 17, y - 40));
   stem.style = {
     strokeColor: "black",
-    strokeWidth: 3
+    strokeWidth: 2
   };
 }
 
 function drawEighth(x,y) {
-  var eighthOne = new Path.Ellipse(new Rectangle(new Point(x ,y), new Size(12, 9))).rotate(-22.5).fillColor = "black";
-  var eighthTwo = new Path.Ellipse(new Rectangle(new Point(x + 20, y), new Size (12, 9))).rotate(-22.5).fillColor = "black";
+  var eighthOne = new Shape.Ellipse(new Rectangle(new Point(x ,y), new Size(18, 14))).rotate(-22.5).fillColor = "black";
+  var eighthTwo = new Shape.Ellipse(new Rectangle(new Point(x + 30, y), new Size (18, 14))).rotate(-22.5).fillColor = "black";
 
-  var stemOne = new Path.Line(new Point(x + 11, y + 2), new Point(x + 11, y - 30)).strokeColor = "#000";
-  var stemTwo = new Path.Line(new Point(x + 31, y + 2), new Point(x + 31, y - 30)).strokeColor = "#000";
+  var stemOne = new Path.Line(new Point(x + 17, y + 6), new Point(x + 17, y - 40));
+  var stemTwo = new Path.Line(new Point(x + 47, y + 6), new Point(x + 47, y - 40));
 
-  var beem = new Path.Line(new Point(x + 11, y - 28), new Point(x + 31, y - 28));
+  var stemGroup = new Group(stemOne, stemTwo);
+  stemGroup.style = {
+    strokeColor: 'black',
+    strokeWidth: 2
+  };
+
+  var beem = new Path.Line(new Point(x + 17, y - 38), new Point(x + 47, y - 38));
   beem.style = {
     strokeColor: "black",
-    strokeWidth: 3
+    strokeWidth: 4
   };
 }
 
 function drawEightSingle(x, y) {
-  var note = new Path.Ellipse(new Rectangle(new Point(x ,y), new Size(12, 9))).rotate(-22.5).fillColor = "black";
+  var note = new Shape.Ellipse(new Rectangle(new Point(x ,y), new Size(18, 14))).rotate(-22.5).fillColor = "black";
 
-  var stem = new Path.Line(new Point(x + 11, y + 2), new Point(x + 11, y - 30)).strokeColor = "#000";
+  var stem = new Path.Line(new Point(x + 17, y + 6), new Point(x + 17, y - 40));
+  stem.style = {
+    strokeColor: 'black',
+    strokeWidth: 2
+  };
 
   var tail = new Path();
-  tail.add(x+11, y-30);
-  tail.curveTo(new Point(x+20, y-10), new Point(x+15, y));
-  tail.strokeColor= "#000";
-  //tail.fillColor = "#000";
+  tail.add(x+17, y-40);
+  tail.curveTo(new Point(x+30, y-20), new Point(x+26, y));
+  tail.style = {
+    strokeColor: 'black',
+    strokeWidth: 2
+  };
 }
 
-for (var i = 0; i <= 13; i++) {
+function drawHalfNote(x, y) {
+  var note = new Shape.Ellipse(new Rectangle(new Point(x, y+1), new Size(15, 10))).rotate(-22.5);
+  note.style = {
+    strokeColor: 'black',
+    strokeWidth: 3
+  };
+
+  var stem = new Path.Line(new Point(x + 15, y + 6), new Point(x + 15, y - 40));
+  stem.style = {
+    strokeColor: "black",
+    strokeWidth: 2
+  };
+}
+
+function drawWholeNote(x, y) {}
+
+for (var i = 0; i <= 11; i++) {
   drawMeasure(measureCount, measureIterator, lineCount);
 }
 
-drawQuarter(40, 242);
+s = 40;
+drawQuarter(s, 212);
+drawQuarter(s + 75, 212);
+drawQuarter(s + 75*2, 212);
+drawQuarter(s + 75*3, 212);
+
+drawEighth(s+73.5*4, 212);
+drawEighth(s+73.5*5, 212);
+drawEighth(s+73.5*6, 212);
+drawEighth(s+73.5*7, 212);
+
+drawHalfNote(s+82*8, 212);
+drawHalfNote(s+87*9, 212);
