4c0f3eee9ee1e4c56fff68676335fd739a02d881
David Reichert <wildcat1221@gmail.com>
2014-04-22 20:40:28 -0500

    Added eighth notes (both), enhanced half and quarter notes, added naturals. Converted whole, half, quarter, and eighth noted to objected and whole and half rests to objects (left orginal code).

--- public/JS/svg.js
+++ public/JS/svg.js
@@ -8,9 +8,9 @@ window.onload = function () {
 
   function startStave() {
 
-    var stave = paper.image("/svg/TrebleClef.svg", 200, 77.75, 31.4985 * 1.5, 77.641 * 1.5);
+    var stave = paper.image("/svg/TrebleClef.svg", 320, 77.75, 31.4985 * 1.5, 77.641 * 1.5);
 
-    var x = 200, y = 148.75, factor = 16.25;
+    var x = 320, y = 148.75, factor = 16.25;
 
     var outline = paper.rect(x, 99.25, 300, 65);
     outline.attr({
@@ -47,7 +47,7 @@ window.onload = function () {
     } else {
       y = 148.75;
       lineY = 100;
-      x = 200 + (300 * measureCount);
+      x = 320 + (300 * measureCount);
     }
 
     if (line > 2) {
@@ -143,27 +143,89 @@ window.onload = function () {
 
 /******** Draw Notes ********/
 
-function drawWholeNote(){
-  var wholeNote = paper.image("/svg/WholeNote.svg", 300, 86.5, 110.5, 76);
+function drawWholeNote(x, y){
+  var wholeNote = paper.image("/svg/WholeNote.svg", x, y, 110.5, 76);
+}
+
+function WholeNote(x, y){
+  this.x = x; // 420
+  this.y = y; // 86.5
+
+  var wholeNote = paper.image("/svg/WholeNote.svg", this.x, this.y, 110.5, 76);
 }
 
 function drawHalfNote() {
-  var halfNote = paper.image("/svg/HalfNote.svg", 600, 95.5, 55, 85);
+  var halfNote = paper.image("/svg/HalfNote.svg", 720, 94, 55, 82);
+}
+
+function HalfNote(x, y) {
+  this.x = x;
+  this.y = y;
+
+  var halfNote = paper.image("/svg/HalfNote.svg", this.x, this.y, 55, 82);
 }
 
 function drawQuaterNote() {
-  var quaterNote = paper.image("/svg/QuaterNote.svg", 900, 95.5, 55, 85);
+  var quaterNote = paper.image("/svg/QuaterNote.svg", 960, 95.5, 55, 85);
 }
 
-function drawEightNotes() {}
+function QuaterNote(x, y) {
+  this.x = x;
+  this.y = y;
 
-function drawEightNote() {}
+  var quaterNote = paper.image("/svg/QuaterNote.svg", this.x, this.y, 55, 85);
+}
+
+function drawEighthNotes() {
+  var partOne = paper.image("/svg/QuaterNote.svg", 1050, 95.5, 55, 85);
+  var partTwo = paper.image("/svg/QuaterNote.svg", 1090, 95.5, 55, 85);
+  var beam = paper.line(1087, 102, 1128.5, 102);
+  beam.attr({
+    stroke: "#000",
+    strokeWidth: 5
+  });
+}
+
+function EighthNotes(x, y) {
+  this.x = x;
+  this.y = y;
+
+  var partOne = paper.image("/svg/QuaterNote.svg", this.x, this.y, 55, 85);
+  var partTwo = paper.image("/svg/QuaterNote.svg", this.x + 40, this.y, 55, 85);
+  var beam = paper.line(this.x + 37, this.y + 6.5, this.x + 78.5, this.y + 6.5);
+  beam.attr({
+    stroke: "#000",
+    strokeWidth: 5
+  });
+}
+
+function drawEighthNote() {
+  var eighthNote = paper.image("/svg/eighthNote.svg", 960, 94, 55, 85);
+}
+
+function EighthNote(x, y) {
+  this.x = x;
+  this.y = y;
+
+  var eighthNote = paper.image("/svg/eighthNote.svg", this.x, this.y, 55, 85);
+}
 
 
 /******** Draw Rests ********/
 
 function drawWholeRest() {
   var wholeRest = paper.rect(165, 266, 20, 5);
+  wholeRest.attr({
+    stroke: "#000",
+    fill: "#000"
+  });
+}
+
+function WholeRest(x, y) {
+  this.x = x;
+  this.y = y;
+
+  var wholeRest = paper.rect(this.x, this.y, 20, 5);
   wholeRest.attr({
     stroke: "#000",
     fill: "#000"
@@ -178,22 +240,42 @@ function drawHalfRest() {
   });
 }
 
+function HalfRest(x, y) {
+  this.x = x;
+  this.y = y;
+
+  var halfRest = paper.rect(this.x, this.y, 20, 5);
+  halfRest.attr({
+    stroke: "#000",
+    fill: "#000"
+  });
+}
+
 function drawQuarterRest() {}
 
-function drawEightRest() {}
+function drawEighthRest() {}
 
 
 /******** Draw Annotations ********/
 
 function drawSharp() {
-  var sharp = paper.image("/svg/sharp.svg", 303, 95, 55, 60);
+  var sharp = paper.image("/svg/sharp.svg", 418, 95, 55, 60);
 }
 
-function drawFlat() {}
+function drawFlat() {
+  var flat = paper.image("/svg/flat.svg", 423, 76, 55, 60);
+}
 
-function drawNatural() {}
+function drawNatural() {
+  var natural = paper.image("/svg/natural.svg", 423, 95, 55, 60);
+}
 
-function drawDot() {}
+function drawDot() {
+  var dot = paper.circle(493, 125, 2.5);
+  dot.attr({
+    fill: "#000"
+  });
+}
 
 function drawSlur() {}
 
@@ -216,17 +298,25 @@ function drawPianos() {}
   * Track Beats per measure
   * Track Measures
   * Track notes, rests, etc for saving in JSON
+  * Draw Notes in mass by storing in array
 *********                      *******/
 
 /*** Test Portion ***/
   startStave();
 
-  drawWholeNote();
-  drawHalfNote();
-  drawQuaterNote();
-  drawWholeRest();
-  drawHalfRest();
-  drawSharp();
+  var wholeNote = new WholeNote(420, 86.5);
+
+  var halfNote = new HalfNote(720, 94);
+
+  var eighthNote = new EighthNote(960, 94);
+
+  var wholeRest = new WholeRest(165, 266);
+
+  var halfRest = new HalfRest(465, 276);
+  drawNatural();
+  drawDot();
+
+  var eighthNotes = new EighthNotes(1050, 95.5);
 
   for (var i = 0; i < 22; i++) {
     addMeasure();
